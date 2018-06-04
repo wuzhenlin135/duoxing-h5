f9483a1476dbac6dc61d1783ed1cf41ad6a3fcae
fanxianwu <fanxianwu@iwalnuts.com>
2018-06-04 11:36:05 +0800

    modify:: order

--- src/views/order/scanOrder.vue
+++ src/views/order/scanOrder.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
-   	Hello
+   <div>
+       <HeadModule :goHome="this.shouldClearHistory"></HeadModule>
+       <div class="with-header">
+       	<div>
+
+       	</div>
+       </div>   	
   </div>
 </template>
 
 <script>
+	import HeadModule from '@/components/head.vue'
 	import {getCheckOrder} from '@/api'
 
 	export default{
 		components:{
-
+			HeadModule
+		},
+		data () {
+			return {
+				shouldClearHistory:1,
+				backImg:'',
+				ticket:{},
+				title:''
+			}
 		},
 		created(){
 			this.getScanOrder()
@@ -18,6 +32,10 @@
 			getScanOrder(){
 				const scenicId = this.$store.getters.scenicId
 				getCheckOrder(scenicId).then(jsonData=>{
+					objData = jsonData.obj
+					backImg = objData.fullImg
+					title = objData.title
+					ticket = objData.ticket
 					console.log(jsonData)
 				})
 			}
